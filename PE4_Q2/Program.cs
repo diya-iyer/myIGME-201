3b82261c41c28c47a4765c1c3e89e8becff34351
thunderbotsftc <52214174+thunderbotsftc@users.noreply.github.com>
2023-09-10 17:48:11 -0700

    PE4 - Question 2 Solution

--- PE4_Q2/Program.cs
+++ PE4_Q2/Program.cs
@@ -6,21 +6,29 @@ using System.Threading.Tasks;
 
 namespace PE4_Q2
 {
-    internal class Program
+    // Class: Program
+    // Author: Diya Iyer
+    // Restrictions: None
+    static internal class Program
     {
+        // Method: Main
+        // Purpose: Get the user to input two numbers and check if it meets the requirements of being less than 10
+        // Restrictions: None
         static void Main(string[] args)
         {
             while (true)
             {
+                // Used a try / catch originally then went with the TryParse method instead, still kept this as a backup
                 try
                 {
                     int numberOne = 0;
                     int numberTwo = 0;
                     while (true)
                     {
+                        // Prompting the user to enter a value
                         Console.WriteLine("Type in the first number: ");
                         string inputOne = Console.ReadLine();
-                        
+                        // Checking if the input is a valid integer and rejecting it if not
                         if (!int.TryParse(inputOne, out numberOne))
                         {
                             Console.WriteLine("The given input must be an integer");
@@ -33,8 +41,10 @@ namespace PE4_Q2
 
                     while (true)
                     {
+                        // Prompting the user to enter a value
                         Console.WriteLine("Type in the second number: ");
                         string inputTwo = Console.ReadLine();
+                        // Checking if the input is a valid integer and rejecting it if not
                         if (!int.TryParse(inputTwo, out numberTwo))
                         {
                             Console.WriteLine("The given input must be an integer");
@@ -44,8 +54,9 @@ namespace PE4_Q2
                             break;
                         }
                     }
-                   
+                   // Displaying the inputted values
                     Console.WriteLine("The given numbers are: " + numberOne + " and " + numberTwo);
+                    // Checking if both values are less than 10 and rejecting them if not
                     if (numberOne > 10 && numberTwo > 10)
                     {
                         Console.WriteLine("The input has been rejected. Both must be numbers that are less than 10");
@@ -55,6 +66,7 @@ namespace PE4_Q2
                         break;
                     }
                 }
+                // If a system error comes up, display this message to the user so they can present a valid input
                 catch (System.FormatException)
                 {
                     Console.WriteLine("The input has been rejected. Both must be numbers that are less than 10");
