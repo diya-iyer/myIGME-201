b05bdb7c82799027544c2dfe19544e8d3f5462d6
thunderbotsftc <52214174+thunderbotsftc@users.noreply.github.com>
2023-09-20 16:47:16 -0700

    Question 7 - PE8

---
+++ PE8_Q7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Q7
+{
+    // class: Program
+    // Author: Diya Iyer
+    // Purpose: The main class hosting the methods
+    // Restrictions: None
+    internal class Program
+    {
+        // Method: Main 
+        // Purpose: Main method managing the user inputted string
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            // Call a method to reverse the string
+            string reversed = ReverseString(input);
+
+            Console.WriteLine("Reversed string: " + reversed);
+
+            // Wait for user input to close the console window
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        // Method: ReverseString
+        // Purpose: Manages reversing the string the user inputs
+        // Restrictions: None
+        static string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    
+    }
+}
