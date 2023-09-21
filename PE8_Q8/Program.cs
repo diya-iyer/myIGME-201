5cd320de11f9ba9d0fbed4f8169fa7d71ca01fd9
thunderbotsftc <52214174+thunderbotsftc@users.noreply.github.com>
2023-09-20 18:03:52 -0700

    PE8-Question8

---
+++ PE8_Q8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Q8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            // Perform case-insensitive replacement
+            string replacedString = ReplaceNoWithYes(input);
+
+            Console.WriteLine("Replaced string: " + replacedString);
+        }
+
+        static string ReplaceNoWithYes(string input)
+        {
+            // Split the input string into words
+            string[] words = input.Split(' ');
+
+            // Iterate through the words and replace "no" with "yes" (case-insensitive)
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (string.Equals(words[i], "no", StringComparison.OrdinalIgnoreCase))
+                {
+                    words[i] = "yes";
+                }
+                else if (string.Equals(words[i], "yes", StringComparison.OrdinalIgnoreCase))
+                {
+                    words[i] = "no";
+                }
+            }
+
+            // Reconstruct the string from the modified words
+            string replacedString = string.Join(" ", words);
+
+            return replacedString;
+
+        }
+    }
+}
