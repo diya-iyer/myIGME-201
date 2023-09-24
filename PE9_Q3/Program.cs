ed432c8c7492f34e84dc01a721784938fabfde7d
thunderbotsftc <52214174+thunderbotsftc@users.noreply.github.com>
2023-09-24 15:42:06 -0700

    PE9 - Q3

---
+++ PE9_Q3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_Q3
+{
+    
+    static internal class Program
+    {
+        // setting up the delegate function
+        delegate string ReadLine();
+
+        // Method : Main 
+        // Author: Diya Iyer
+        // Purpose: To use the delegate function to replace the Console.ReadLine() functionality
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            // Create an instance of the delegate and assign it to Console.ReadLine
+            ReadLine readLineDelegate = Console.ReadLine;
+
+            Console.WriteLine("Enter something:");
+
+            // Use the delegate to read user input
+            string userInput = readLineDelegate();
+
+            // Print the user input
+            Console.WriteLine($"You entered: {userInput}");
+        }
+    }
+}
