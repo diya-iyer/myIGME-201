5c8c0274eddb95e1f299f9f7a07993856f319a66
thunderbotsftc <52214174+thunderbotsftc@users.noreply.github.com>
2023-10-01 15:44:23 -0700

    Question 4 - Unit 1 Exam

---
+++ Unit1Exam_Q4/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Unit1Exam_Q4
+{
+    // Class: Program
+    // Author: Diya Iyer
+    // Purpose: Asking 3 different questions to the user
+    // Restrictions: None
+    static class Program
+    {
+        // Checking if the user got timed out
+        static bool bTimedOut = false;
+        // string for response
+        static string sResponse = "";
+
+        // initialize timer for 5 seconds
+        static System.Timers.Timer questionTimer = new System.Timers.Timer(5000);
+
+        // Method: Main()
+        // Author: Diya Iyer
+        // Purpose: Prompting the user to answer a set of questions with predetermined answers under a time limit
+        // Restrictions: User must select a question between 1-3
+        static void Main()
+        {
+
+            // string and int of # of the question to be asked
+            string sQuestions = "";
+            int nQuestions = 0;
+
+            // string and int of answers
+            string sResponse = "";
+
+            // declaring the arrays of questions and answers
+            string[] questionArray = { "What is your favorite color?", "What is the answer to life, the universe, and everything?", "What is the airspeed velocity of an unladen swallow?" };
+            string[] answerArray = { "black", "42", "What do you mean? African or European swallow?" };
+
+            // play again?
+            string sAgain = "";
+
+            // valid state
+            bool bValid = false;
+
+            questionTimer.Elapsed += new ElapsedEventHandler(QuestionTimeout);
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+        // label to return to if they want to play again
+        start:
+
+
+            // ask which question the user wishes to answer until they provide a valid input
+            do
+            {
+                Console.Write("Choose your question (1-3): ");
+                sQuestions = Console.ReadLine();
+                bValid = int.TryParse(sQuestions, out nQuestions);
+                if (bValid)
+                {
+                    if (nQuestions < 1 || nQuestions > 3)
+                    {
+                        Console.WriteLine("Please enter a number between 1 - 3");
+                        bValid = false;
+                        continue;
+                    }
+                }
+            } while (!bValid);
+
+            Console.WriteLine();
+            Console.WriteLine("You have 5 seconds to answer the following question: ");
+
+            // assigning values based on validity 
+            bValid = false;
+            bTimedOut = false;
+
+            // display the question and prompt for the answer until they enter a valid number
+            do
+            {
+                Console.WriteLine(questionArray[nQuestions - 1]);
+                // start the timer
+                questionTimer.Start();
+                sResponse = Console.ReadLine();
+                if (bTimedOut)
+                {
+                    // reset values
+                    bValid = true;
+                    questionTimer.Stop();
+                }
+                else
+                {
+                    // check for input length 
+                    bValid = sResponse.Length > 0;
+
+                    if (!bValid)
+                    {
+                        Console.WriteLine("Please provide an answer");
+                        questionTimer.Stop();
+                    }
+                }
+
+            } while (!bValid);
+
+            // if response == answer, congratulate the user
+            if (sResponse.CompareTo(answerArray[nQuestions - 1]) == 0)
+            {
+                Console.WriteLine("Well done!");
+            }
+            // else tell the user if they gave the wrong answer or ran out of time
+            else
+            {
+                if (bTimedOut)
+                {
+                    Console.WriteLine("Time's up!");
+                    Console.WriteLine("The answer is: " + answerArray[nQuestions - 1]);
+                    bTimedOut = false;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong! The answer is: " + answerArray[nQuestions - 1]);
+                }
+            }
+
+            Console.WriteLine("Please press enter.");
+
+            do
+            {
+                // prompt if they want to play again
+                Console.Write("Play again?");
+
+                sAgain = Console.ReadLine();
+
+                // if they type y or yes then play again
+                // else if they type n or no then leave this loop
+                if (sAgain.ToLower().Trim().StartsWith("y"))
+                {
+                    goto start;
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            } while (true);
+        }
+
+        // Method: QuestionTimeout
+        // Author: Diya Iyer
+        // Purpose: To check if the user has run out of time to answer
+        // Restrictions: None
+        static void QuestionTimeout(object source, ElapsedEventArgs e)
+        {
+            if (sResponse == null || sResponse.Length == 0)
+            {
+                bTimedOut = true;
+            }
+        }
+    }
+}
---
+++ Unit1Exam_Q8/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit1Exam_Q8
+{
+    internal class Program
+    {
+        // declaring the delegate function 
+        delegate double RoundedValue(double value, int decimalPlace);
+        static void Main(string[] args)
+        {
+            // Define the range and step size for x and y
+            double minX = 0;
+            double maxX = 4;
+            double minY = -1;
+            double maxY = 1;
+            double step = 0.1;
+
+            // Calculate the number of data points in each dimension
+            int numXPoints = (int)((maxX - minX) / step) + 1;
+            int numYPoints = (int)((maxY - minY) / step) + 1;
+
+            // Create a 2D array to store the values of z
+            double[,] zValues = new double[numYPoints, numXPoints];
+
+            // declare the delegate method variable
+            RoundedValue roundingResult = new RoundedValue(RoundingOperation);
+
+            // Loop through the values of x and y
+            for (int i = 0; i < numYPoints; i++)
+            {
+                double y = minY + i * step;
+                y = roundingResult(y, 1);
+                for (int j = 0; j < numXPoints; j++)
+                {
+                    double x = minX + j * step;
+                    x = roundingResult(x, 1);
+
+
+                    // Calculate the value of z using the formula
+                    double z = 4 * Math.Pow(y, 3) + 2 * Math.Pow(x, 2) - 8 * x + 7;
+                    z = roundingResult(z, 3);
+
+                    // Store the value of z in the array
+                    zValues[i, j] = z;
+                }
+            }
+        }
+        // Method: RoundingOperations 
+        // Author: Diya Iyer
+        // Purpose: The method that rounds the inputted number to the desried decimal place
+        // Restrictions: Returns a double
+        static double RoundingOperation(double value, int decimalPlace)
+        {
+            return Math.Round(value, decimalPlace);
+        }
+    }
+}
