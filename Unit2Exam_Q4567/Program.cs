73d50b9f999823570f877927ebf1b73c1fcd5aa2
thunderbotsftc <52214174+thunderbotsftc@users.noreply.github.com>
2023-10-27 19:13:52 -0400

    number4 progress

---
+++ Unit2Exam_Q4567/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit2Exam_Q4567
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+
+        public string PhoneNumber;
+
+        public abstract void Connect();
+
+
+        public abstract void Disconnect();
+       
+    }
+
+    public interface PhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+    }
+
+    public class RotaryPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+        public override void Connect()
+        {
+            
+        }
+
+        public override void Disconnect()
+        {
+            
+        }
+    }
+
+    public class PushButtonPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    public class Tardis : RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+
+        public byte WhichDrWho { set { whichDrWho = value; } }
+        public string FemaleSideKick { set { femaleSideKick = value; } }
+        public void TimeTravel()
+        {
+
+        }
+    }
+
+    public class PhoneBooth : PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor()
+        {
+
+        }
+
+        public void CloseDoor()
+        {
+
+        }
+    }
+}
---
+++ testfile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testfile
+{
+   
+    static class Program
+    {
+        static void Main()
+        {
+
+            SortedList<string, DateTime> friendBirthdays = new SortedList<string, DateTime>();
+            friendBirthdays["Diya"] = new DateTime(2004, 6, 10);
+            friendBirthdays["Teddy"] = new DateTime(2015, 3, 5);
+            friendBirthdays["friend3"] = new DateTime(2004, 11, 27);
+
+            foreach (KeyValuePair<string, DateTime> kvp in friendBirthdays)
+            {
+                Console.WriteLine(kvp.Key + "'s birthday is: " + kvp.Value.ToString("MM/dd/yyyy"));
+            }
+
+
+
+        }
+    }
+
+}
