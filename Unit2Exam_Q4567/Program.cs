cb8a3395a503c44dea004f9548a3a1c4d64b6449
thunderbotsftc <52214174+thunderbotsftc@users.noreply.github.com>
2023-10-29 21:55:09 -0400

    Questions 4, 5, 6, 7 - Unit Exam 2

--- Unit2Exam_Q4567/Program.cs
+++ Unit2Exam_Q4567/Program.cs
@@ -7,10 +7,18 @@ using System.Threading.Tasks;
 
 namespace Unit2Exam_Q4567
 {
+    // Class: Program
+    // Author: Diya Iyer
+    // Purpose: Hosting the Main method and UsePhone method
+    // Restrictions: None
     internal class Program
     {
+        // Method: Main
+        // Purpose: Calling UsePhone method with the Tardis and PhoneBooth objects
+        // Restrictions: None
         static void Main(string[] args)
         {
+            // creating new objects to pass through UsePhone
             Tardis myTardis = new Tardis();
             PhoneBooth phoneBooth = new PhoneBooth();
 
@@ -18,6 +26,9 @@ namespace Unit2Exam_Q4567
             UsePhone(phoneBooth);
         }
 
+        // Method: UsePhone
+        // Purpose: Calling object's methods through interface as well as object specific ones
+        // Restrictions: None
         static void UsePhone(object obj)
         {
             if (obj is PhoneInterface iPhone)
@@ -40,6 +51,8 @@ namespace Unit2Exam_Q4567
             }
         }
     }
+
+    // Below is the converted schUML to C# code for Question 4
 
     public abstract class Phone
     {
@@ -130,6 +143,7 @@ namespace Unit2Exam_Q4567
 
         }
 
+        // Overloads all the stated bool operators and makes sure the value of 10 in whichDrWho evaluates to greater than
         public static bool operator ==(Tardis t1, Tardis t2)
         {
             return t1.whichDrWho == t2.whichDrWho;
